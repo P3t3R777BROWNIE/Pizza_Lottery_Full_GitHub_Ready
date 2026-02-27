1805c1f7261f7d5a769723e6c410927aab38086b
P3t3R777BROWNIE <Peterebay101@gmail.com>
2026-02-27 20:36:06 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pizza_Lottery_Full_GitHub_Ready
+pizza lottery for a £1 subscription fee for a chance to win a £20 meal deal from dominoes
---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return (
+    <div style={{fontFamily: "Arial", padding: "40px"}}>
+      <h1>🍕 Pizza Lottery</h1>
+      <p>Your subscription pizza lottery platform scaffold is ready.</p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
